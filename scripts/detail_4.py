62f0e7e55f6b046766420c99a8c151863bbf5bdf
QA10times <qa@10times.com>
2025-08-13 18:39:10 +0530

    Update detail_4.py

--- scripts/detail_4.py
+++ scripts/detail_4.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, sync_playwright, Error
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    browser = playwright.chromium.launch(headless=True, slow_mo=500)
     context = browser.new_context()
     page = context.new_page()
 
