d90686b77c57d250d04aad9ae3c492386666e6c1
QA10times <qa@10times.com>
2025-09-16 18:17:56 +0530

    Update tracker_24.py

--- scripts/tracker_24.py
+++ scripts/tracker_24.py
@@ -37,7 +37,7 @@ def run(playwright: Playwright) -> None:
     page.get_by_text("Map").click()
     time.sleep(2)
     page.get_by_role("button", name="Event List").click()
-    page.get_by_text("1", exact=True).nth(2).click()
+    page.get_by_text("1", exact=True).nth(3).click()
     page.get_by_text("Calendar").click()
     page.get_by_text("Table").click()
     page.get_by_role("button", name="Upcoming Events").click()
