23ab51ae1bdd0abdf52c621847fdbc9be57493bd
QA10times <qa@10times.com>
2025-11-01 15:13:00 +0530

    Update tracker_59.py

--- scripts/tracker_59.py
+++ scripts/tracker_59.py
@@ -31,7 +31,7 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("button", name="Save").click()
     time.sleep(5)
     page.get_by_role("button", name="Skip").click()
-    page.get_by_role("button", name="Past Events").click()
+    page.get_by_role("button", name="Upcoming Events").click()
     page.get_by_text("Table").click()
     time.sleep(5)
     page.get_by_text("Est. Visitors").click()
@@ -47,7 +47,7 @@ def run(playwright: Playwright) -> None:
     page.get_by_role("radio", name="Growing").click()
     page.get_by_role("button", name="Save").click()
     time.sleep(5)
-    page.get_by_role("button", name="Past Events").click()
+    page.get_by_role("button", name="Upcoming Events").click()
     page.get_by_text("Table").click()
     page.get_by_text("Maturity").click()
 
