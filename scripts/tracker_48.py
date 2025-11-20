2b388f830d6de749f4bc183a576a27ba734f9e39
QA10times <qa@10times.com>
2025-11-20 14:51:53 +0530

    Update tracker_48.py

--- scripts/tracker_48.py
+++ scripts/tracker_48.py
@@ -16,7 +16,7 @@ def run(playwright: Playwright) -> None:
     time.sleep(3)
     page.get_by_role("button", name="Create").click()
     page.get_by_role("button", name="Skip").click()
-    page.get_by_role("link", name="Competitor Track your competitor").click()
+    page.get_by_role("link", name="people template Company &").click()
     page.get_by_role("button", name="Skip").click()
     page.get_by_role("textbox", name="Competitor Name or Website").click()
     page.get_by_role("textbox", name="Competitor Name or Website").fill("cvent")
