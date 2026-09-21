b9c9711cda7cf1184193f850880e0b8fd7f9794c
QA10times <qa@10times.com>
2026-09-21 10:26:29 +0530

    add

--- scripts/onboarding_9.py
+++ scripts/onboarding_9.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
@@ -8,6 +10,7 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://gtm.whr.ai/signin")
     page.goto("https://gtm.whr.ai/login?utm_source=10times&utm_medium=web&utm_campaign=right_rail&hash=4IQjAPckGZDk9ArLj1D3pDYc8tvqFPX7ZsemflFWON0=&uid=1048476&platform=gtm")
+    time.sleep(5)
     page.mouse.move(0, 500)
     page.get_by_role("button", name="NC nishant choudhary").click()
     page.get_by_role("button", name="Logout").first.click()
